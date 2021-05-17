97470017fcbc31369b6b569b11745cc27ba48e93
Morin <morin.alexandre1@gmail.com>
2021-05-17 12:53:44 +0200

    function retrieveCameras added

--- front-end/basket.js
+++ front-end/basket.js
@@ -26,3 +26,29 @@ function countBasket(){
         numberOfProducts.textContent = basket.length;
     }
 };
+
+function retrieveCameras () {
+    let monobjet = localStorage.getItem('basket');
+    console.log(monobjet);
+    let monObjet = JSON.parse(monobjet);
+    console.log(monObjet);
+    console.log(localStorage.key(0));
+    console.log(monObjet[1]);
+    monObjet.forEach(element => {
+        const camerasWrapper = document.getElementById("cameras2");
+        var ghost2 = document.getElementById("ghost2");
+        var cameraWrapper = ghost2.cloneNode(true);
+        cameraWrapper.style = null;
+        var img = cameraWrapper.getElementsByTagName('img').item(0);
+        var nameCam = cameraWrapper.getElementsByTagName('h2').item(0);
+        var priceCam = cameraWrapper.getElementsByTagName('p').item(0);
+        var link = cameraWrapper.getElementsByTagName('a').item(0);
+        img.setAttribute("src", element.imageUrl);
+        var productId = element._id;
+        link.setAttribute("href", "./produit.html?id="+productId);
+        nameCam.textContent = element.name;
+        priceCam.textContent = element.price;
+        camerasWrapper.appendChild(cameraWrapper);
+    });
+}
+            
--- front-end/panier.js
+++ front-end/panier.js
@@ -2,8 +2,8 @@
 const emptyBasket = document.getElementById('emptyBasket');
 emptyBasket.addEventListener("click", () => {
     localStorage.clear();
-    //window.location = "file:///Users/Alex/Desktop/Orinoco/front-end/index.html"
 }, false);
+
 
 fetch("http://localhost:3000/api/cameras")
 .then(res => {
@@ -13,13 +13,7 @@ fetch("http://localhost:3000/api/cameras")
             countBasket();         
             console.log(data);
             console.log(localStorage);
-            //localStorage.setItem(JSON.stringify(data.name), JSON.stringify(data));
-            /*console.log(localStorage);
-            let monobjet = localStorage.getItem('\"camera\"');
-            console.log(monobjet);
-            let monObjet = JSON.parse(monobjet);
-            console.log(monObjet);
-            console.log(localStorage.key(0));*/
+            retrieveCameras();
         })
     } else {
         console.log("ERREUR");
